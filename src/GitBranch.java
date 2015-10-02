29efa3b5087aff73e0704afc8c9cadb26fb38f8c
Noah Camp <campn@student.swosu.edu>
2015-10-02 10:26:55 -0500

    made program more friendly

--- src/GitBranch.java
+++ src/GitBranch.java
@@ -7,6 +7,8 @@ public class GitBranch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
+		
+		System.out.println("Today is a beautiful day");
 	}
 
 }
